8526d0a753d473d3c218a33f914c583b2e2d7844
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-09-13 15:14:56 +0530

    chore:Db cloned

--- util/db.js
+++ util/db.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
-export const connect = (url = process.env.MONGO_CONNECTION_STRING) => {
-  return mongoose.connect(url, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true,
-  });
+
+export const connect = async (url = process.env.MONGO_CONNECTION_STRING) => {
+  try {
+    await mongoose.connect(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log("Connection to Beameri MongoDB successful!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 };
+
